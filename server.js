d9cab4b10e8903c611e7fb5784d26298967e06b3
erickk474 <erickk474@gmail.com>
2020-08-30 02:35:42 -0300

    Mudança na inicialização + atualização de dependencias - Nodemon

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const initDb = require('./src/services/initializeDatabase');
+const dataBase = require('./src/services/initializeDatabase');
 const bodyParser = require('body-parser');
-
-require('dotenv/config');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-initDb.initializeDatabase();
+dataBase.initializeDatabase();
 
 app.use('/api', require('./src/routes'));
 
