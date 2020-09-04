7c1bbc10c6990106dd951697bf2a100206a7e86b
erickk474 <erickk474@gmail.com>
2020-09-04 02:57:14 -0300

    rota de pagamento adicionada

--- server.js
+++ server.js
@@ -13,5 +13,6 @@ dataBase.initializeDatabase();
 app.use('/api/products', require('./src/routes/ProductRouter'));
 app.use('/api/customers', require('./src/routes/CustomerRouter'));
 app.use('/api/categories', require('./src/routes/CategoryRouter'));
+app.use('/api/payments', require('./src/routes/PaymentsRouter'));
 
 app.listen(process.env.PORT || 3000);
